631c52a18eba5b9ddadcc62a9ec81439bf0cfaf5
Igor Rais <riv82.ios@gmail.com>
2021-12-05 15:53:50 +0500

    Project creation.

---
+++ GBShop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GBShop
+//
+//  Created by Igor Rais on 05.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
