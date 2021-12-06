226f5ebd5706c6f35207fa7882dfb45b9f0bf396
Igor Rais <riv82.ios@gmail.com>
2021-12-07 00:55:21 +0500

    Added stub files to empty project folders.

---
+++ GBShop/App/BusinessLogic/BusinessLogicStub.swift
@@ -0,0 +1,8 @@
+//
+//  businessLogic_stub.swift
+//  GBShop
+//
+//  Created by Igor Rais on 07.12.2021.
+//
+
+import Foundation
---
+++ GBShop/Core/CoreStub.swift
@@ -0,0 +1,8 @@
+//
+//  CoreStub.swift
+//  GBShop
+//
+//  Created by Igor Rais on 07.12.2021.
+//
+
+import Foundation
